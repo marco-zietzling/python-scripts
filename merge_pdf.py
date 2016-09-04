51c2593c21d503c70b00a33fe47aaa3a39ba4e0d
Marco Zietzling <marco.zietzling@gmail.com>
2016-09-04 22:55:14 +0200

    added check for PDF files

--- merge_pdf.py
+++ merge_pdf.py
@@ -20,11 +20,12 @@ files = os.listdir(working_directory)
 # print(working_directory)
 # print(files)
 
-# iterate over files and append them
+# iterate over files and append them (consider only PDF files)
 for file in files:
-	print("processing " + file)
-	input = open(working_directory + "/" + file, "rb")
-	merger.append(input)
+	if file.endswith(".pdf"):
+		print("processing " + file)
+		input = open(working_directory + "/" + file, "rb")
+		merger.append(input)
 
 # write output to new file
 output = open(working_directory + "/output.pdf","wb")
