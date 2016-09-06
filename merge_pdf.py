b541a0e3bcc28dfe6357f1d6c0105d58390c342a
Marco Zietzling <marco.zietzling@gmail.com>
2016-09-06 23:24:38 +0200

    refactor to be more pythonic

--- merge_pdf.py
+++ merge_pdf.py
@@ -1,36 +1,49 @@
-# merge several PDF files from a directoy to a new file called 'output.pdf'
-# the first argument defines the directory
-# PDF files are merged according to alphabetical order
-# usage: python merge_pdf.py path/to/directoy/containing/pdf/files
+"""Merge several PDF files within a given directoy to a new file called 'output.pdf'.
+The first argument defines the directory.
+PDF files are merged according to alphabetical order.
+Usage: python merge_pdf.py path/to/directoy/containing/pdf/files
+"""
 
 import os
-from PyPDF2 import PdfFileMerger
 import sys
 import warnings
+from PyPDF2 import PdfFileMerger
 
-# ignore all warnings (e.g. PDF read warnings)
+
+def is_valid_directory(argument):
+    """Sanity check argument to be an existing directoy.
+    """
+
+    condition1 = argument is not None
+    condition2 = os.path.isdir(argument)
+    return condition1 and condition2
+
+
+def merge_pdf_files_in_directory(directory):
+    """Merge all PDF files from given directory to a new file called 'output.pdf'.
+    """
+
+    merger = PdfFileMerger()
+    files = os.listdir(directory)
+
+    # Iterate over files and append them (consider only PDF files).
+    for file in files:
+        if file.lower().endswith(".pdf"):
+            print("processing " + file)
+            inputfile = os.path.join(directory, file)
+            inputfile = os.path.normcase(inputfile)
+            source_pdf = open(inputfile, "rb")
+            merger.append(source_pdf)
+
+    # Write output to new file.
+    outputfile = os.path.join(directory, "output.pdf")
+    target_pdf = open(outputfile, "wb")
+    merger.write(target_pdf)
+    target_pdf.close()
+
+
+# Ignore all warnings (e.g. PDF read warnings).
 warnings.filterwarnings("ignore")
 
-# define the PDF merger
-merger = PdfFileMerger()
-
-# get the working directory and all files in it
-working_directory = sys.argv[1]
-files = os.listdir(working_directory)
-# print(working_directory)
-# print(files)
-
-# iterate over files and append them (consider only PDF files)
-for file in files:
-    if file.lower().endswith(".pdf"):
-        print("processing " + file)
-        filepath = os.path.join(working_directory, file)
-        filepath = os.path.normcase(filepath)
-        input = open(filepath, "rb")
-        merger.append(input)
-
-# write output to new file
-outputfile = os.path.join(working_directory, "output.pdf")
-output = open(outputfile,"wb")
-merger.write(output)
-output.close()
+if is_valid_directory(sys.argv[1]):
+    merge_pdf_files_in_directory(sys.argv[1])
